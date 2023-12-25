4a3ae583d4547e4af540821fdb9173464957116c
mustafa-barakzey <mustafabarakzey@gmail.com>
2023-12-25 09:47:25 -0800

    اضافه کردن متدهایی برای ولیدیشن در لایه base برای اینکه به لایه های دیگه وابسته نشه

---
+++ 01_Framework/brk.Framework.Base/Utils/RegexPattern.cs
@@ -0,0 +1,15 @@
+﻿namespace brk.Framework.Base.Constants
+{
+    internal class RegexPattern
+    {
+        /// <summary>
+        /// پترن ولیدیشن برای موبایل
+        /// </summary>
+        public const string MobilePattern = @"(\+989|00989|989|09|\+۹۸۹|۰۰۹۸۹|۹۸۹|۰۹)[0-9,۰,١,۲,۳,۴,۵,۶,۷,۸,۹]{9,9}$";
+
+        /// <summary>
+        /// پترن ولیدیشن برای ایمیل
+        /// </summary>
+        public const string EmailPattern = @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";
+    }
+}
