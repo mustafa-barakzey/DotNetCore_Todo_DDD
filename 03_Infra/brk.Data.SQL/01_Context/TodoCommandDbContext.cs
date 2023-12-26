af36080f3e3001584eb15968df569932e26c54fc
mustafa-barakzey <mustafabarakzey@gmail.com>
2023-12-26 11:26:42 -0800

    اضافه کردن لایه دیتا برای دسترسی به دیتابیس

---
+++ 03_Infra/brk.Data.SQL/01_Context/TodoCommandDbContext.cs
@@ -0,0 +1,24 @@
+﻿using brk.Core.Domain.User.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace brk.Data.SQL._01_Context
+{
+    public class TodoCommandDbContext:DbContext
+    {
+        public TodoCommandDbContext(DbContextOptions<TodoCommandDbContext> options):base(options){}
+
+        #region properties
+
+
+        #endregion
+
+        #region configuration
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #endregion
+    }
+}
