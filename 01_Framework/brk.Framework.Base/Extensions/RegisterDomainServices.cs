bd7c1a9c729bd1e2bd77ee807412502a3145b48a
mustafa-barakzey <mustafabarakzey@gmail.com>
2023-12-26 12:15:02 -0800

    خودکار سازی تزریق وابستگی کلاس هایی که از BaseCommandHandler ارث بری کرده اند

---
+++ 01_Framework/brk.Framework.Base/Extensions/RegisterDomainServices.cs
@@ -0,0 +1,39 @@
+﻿using brk.Framework.Base.Application;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+using System.Reflection;
+
+namespace brk.Framework.Base.Extensions
+{
+    public static class RegisterDomainServices
+    {
+        public static void AddFrameworkBaseService(this IServiceCollection services, string[] assemblyName)
+        {
+            services.AddScoped<ICommandDispatcher, CommandDispatcher>();
+            services.Scan(s => s.FromAssemblies(GetAssemblies(assemblyName))
+                .AddClasses(c => c.AssignableToAny(typeof(ICommandHandler<>), typeof(ICommandHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+        }
+
+        private static List<Assembly> GetAssemblies(string[] assemblyName)
+        {
+            var assemblies = new List<Assembly>();
+            var dependencies = DependencyContext.Default.RuntimeLibraries;
+            foreach (var library in dependencies)
+            {
+                if (IsCandidateCompilationLibrary(library, assemblyName))
+                {
+                    var assembly = Assembly.Load(new AssemblyName(library.Name));
+                    assemblies.Add(assembly);
+                }
+            }
+            return assemblies;
+        }
+        private static bool IsCandidateCompilationLibrary(RuntimeLibrary compilationLibrary, string[] assemblyName)
+        {
+            return assemblyName.Any(d => compilationLibrary.Name.Contains(d))
+                   || compilationLibrary.Dependencies.Any(d => assemblyName.Any(c => d.Name.Contains(c)));
+        }
+    }
+}
--- 04_Endpoints/brk.Endpoints.WebApi/Extensions/RegisterServices.cs
+++ 04_Endpoints/brk.Endpoints.WebApi/Extensions/RegisterServices.cs
@@ -1,18 +1,20 @@
-﻿using brk.Core.Application.User.Commands;
-using brk.Core.Domain.User.Data;
+﻿using brk.Core.Domain.User.Data;
 using brk.Data.SQL._01_Context;
 using brk.Data.SQL.User.Data;
+using brk.Framework.Base.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace brk.Endpoints.WebApi.Extensions
 {
     public static class RegisterServices
     {
-        public static void BooStrapServices(this IServiceCollection services,IConfiguration configuration)
+        public static void BooStrapServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<TodoCommandDbContext>(option => option.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+
+            string[] assemblyList = { "brk.Core.Application", "brk.Data.SQL" };
+            services.AddFrameworkBaseService(assemblyList);
             services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<RegisterUserCommandHandler>();
         }
     }
 }
