a703640a14ac1ec0251faa685d811f0aa3e6561c
mustafa-barakzey <mustafabarakzey@gmail.com>
2023-12-29 02:02:05 -0800

    اضافه کردن لاگین به Swagger

--- 04_Endpoints/brk.Endpoints.WebApi/Extensions/JwtAuthentication.cs
+++ 04_Endpoints/brk.Endpoints.WebApi/Extensions/JwtAuthentication.cs
@@ -1,6 +1,7 @@
 ﻿using brk.Endpoints.WebApi.Services.JWT;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace brk.Endpoints.WebApi.Extensions
@@ -34,6 +35,38 @@ namespace brk.Endpoints.WebApi.Extensions
                     ValidAudience = setting.Audience,
                     IssuerSigningKey = new SymmetricSecurityKey(Key)
                 };
+            });
+        }
+
+        public static void AddAppSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "My API",
+                    Version = "v1"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                   {
+                     new OpenApiSecurityScheme
+                     {
+                       Reference = new OpenApiReference
+                       {
+                         Type = ReferenceType.SecurityScheme,
+                         Id = "Bearer"
+                       }
+                      },
+                      new string[] { }
+                    }
+                  });
             });
         }
     }
