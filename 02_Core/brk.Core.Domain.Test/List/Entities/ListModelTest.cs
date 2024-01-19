6e5e878e075dd1f1853b5d3b44111970f99a4e74
mustafa-barakzey <mustafabarakzey@gmail.com>
2024-01-19 03:52:02 -0800

    add factory_should_create_new_list test

---
+++ 02_Core/brk.Core.Domain.Test/List/Entities/ListModelTest.cs
@@ -0,0 +1,23 @@
+﻿using brk.Core.Domain.List.Entities;
+using brk.Core.Domain.List.ValueObjects;
+using brk.Framework.Base.ValueObjects;
+using FluentAssertions;
+
+namespace brk.Core.Domain.Test.List.Entities
+{
+    public class ListModelTest
+    {
+        [Fact]
+        public void factory_should_create_new_list()
+        {
+            var ownerId=ListOwnerId.FromLong(3);
+            var title=Title.FromString("Title");
+            var model = ListModel.Create(ownerId, title);
+
+            model.Should().NotBeNull();
+            model.OwnerId.Should().Be(ownerId.Value);
+            model.Title.Should().Be(title);
+        }
+
+    }
+}
