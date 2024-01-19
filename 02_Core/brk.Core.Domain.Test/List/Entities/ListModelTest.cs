728323f5c5cb5cd9fb27ed72f7969bda906e055b
mustafa-barakzey <mustafabarakzey@gmail.com>
2024-01-19 03:56:47 -0800

    add set_order_should_update_list_order & update_should_update_list_title tests

--- 02_Core/brk.Core.Domain.Test/List/Entities/ListModelTest.cs
+++ 02_Core/brk.Core.Domain.Test/List/Entities/ListModelTest.cs
@@ -7,6 +7,12 @@ namespace brk.Core.Domain.Test.List.Entities
 {
     public class ListModelTest
     {
+        private readonly ListBuilder _listBuilder;
+        public ListModelTest()
+        {
+            _listBuilder = new ListBuilder();
+        }
+
         [Fact]
         public void factory_should_create_new_list()
         {
@@ -19,5 +25,25 @@ namespace brk.Core.Domain.Test.List.Entities
             model.Title.Should().Be(title);
         }
 
+        [Fact]
+        public void set_order_should_update_list_order()
+        {
+            var model = _listBuilder.Build();
+
+            const int order = 100;
+            model.SetOrder(order);
+            model.Order.Should().Be(order);
+        }
+
+        [Fact]
+        public void update_should_update_list_title()
+        {
+            var model = _listBuilder.Build();
+
+            var title = Title.FromString("new Title");
+
+            model.Update(title);
+            model.Title.Should().Be(title);
+        }
     }
 }
---
+++ 02_Core/brk.Core.Domain.Test/List/ListBuilder.cs
@@ -0,0 +1,14 @@
+﻿using brk.Core.Domain.List.Entities;
+using brk.Core.Domain.List.ValueObjects;
+using brk.Framework.Base.ValueObjects;
+
+namespace brk.Core.Domain.Test.List
+{
+    internal class ListBuilder
+    {
+        ListOwnerId ownerId = ListOwnerId.FromLong(3);
+        Title title = Title.FromString("Title");
+
+        public ListModel Build() => ListModel.Create(ownerId, title);
+    }
+}
