87f3f24fedde735a17195465dc0266df5a455277
mustafa-barakzey <mustafabarakzey@gmail.com>
2024-01-19 04:03:32 -0800

    add factory_should_create_new_task test

---
+++ 02_Core/brk.Core.Domain.Test/TaskList/Entities/TaskModelTest.cs
@@ -0,0 +1,21 @@
+﻿using brk.Core.Domain.TaskList.Entities;
+using brk.Framework.Base.ValueObjects;
+using FluentAssertions;
+
+namespace brk.Core.Domain.Test.TaskList.Entities
+{
+    public class TaskModelTest
+    {
+        [Fact]
+        public void factory_should_create_new_task()
+        {
+            Title title= Title.FromString("task Title");
+            var task = TaskModel.Create(title);
+
+            task.Should().NotBeNull();
+            task.Title.Should().Be(title);
+            task.Status.Should().Be(Status.NotCompleted);
+            task.Category.Should().Be(Category.None);
+        }
+    }
+}
