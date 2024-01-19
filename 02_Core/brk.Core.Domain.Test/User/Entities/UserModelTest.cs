27a3ae9ddb15c98e41cc58403b07652f94c57f57
mustafa-barakzey <mustafabarakzey@gmail.com>
2024-01-19 02:38:55 -0800

    create first unit test user model factory_should_create_new_user

---
+++ 02_Core/brk.Core.Domain.Test/User/Entities/UserModelTest.cs
@@ -0,0 +1,27 @@
+﻿using brk.Core.Domain.User.Entities;
+using brk.Core.Domain.User.ValueObjects;
+using brk.Framework.Base.ValueObjects;
+using FluentAssertions;
+
+namespace brk.Core.Domain.Test.User.Entities
+{
+    public class UserModelTest
+    {
+        [Fact]
+        public void factory_should_create_new_user()
+        {
+            FirstName firstName = FirstName.FromString("mustafa");
+            LastName lastName = LastName.FromString("barakzey");
+            Password password = Password.FromString("Password");
+            Email email = Email.FromString("mustafabarakzey@gmail.com");
+
+            var userModel = UserModel.Create(firstName, lastName, email, password);
+            
+            userModel.Should().NotBeNull();
+            userModel.FirstName.Should().Be(firstName);
+            userModel.LastName.Should().Be(lastName);
+            userModel.Email.Should().Be(email);
+            userModel.Password.Should().Be(password);
+        }
+    }
+}
