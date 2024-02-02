78b027576136fe0d4b7388474591dabcdd02915b
Mustafa Barakzey <mustafabarakzey@gmail.com>
2024-02-02 04:22:34 -0800

    add another test for editListCommandHandler

--- 02_Core/brk.Core.Application.Test/List/Commands/EditListCommandHandlerTest.cs
+++ 02_Core/brk.Core.Application.Test/List/Commands/EditListCommandHandlerTest.cs
@@ -7,18 +7,20 @@ using brk.Framework.Base.ValueObjects;
 using brk.Framework.Base.Web.Services;
 using FluentAssertions;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 
 namespace brk.Core.Application.Test.List.Commands
 {
     public class EditListCommandHandlerTest
     {
         private readonly IListRepository _listRepository;
+        private readonly IUserInfoService _userInfoService;
         private readonly EditListCommandHandler _handler;
         public EditListCommandHandlerTest()
         {
             _listRepository = Substitute.For<IListRepository>();
-            var userInfo = Substitute.For<IUserInfoService>();
-            _handler = new EditListCommandHandler(_listRepository, userInfo);
+            _userInfoService = Substitute.For<IUserInfoService>();
+            _handler = new EditListCommandHandler(_listRepository, _userInfoService);
         }
 
         [Fact]
@@ -39,6 +41,43 @@ namespace brk.Core.Application.Test.List.Commands
             listModel.Update(newTitle);
             listModel.Title.Should().Be(newTitle);
             //result.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task handler_should_return_error_when_list_notFound()
+        {
+            const long userId = 1;
+            var command = new EditListCommand
+            {
+                Id = 1,
+                Title = "new title"
+            };
+            var listModel = ListModel.Create(ListOwnerId.FromLong(1), Title.FromString("title"));
+            _userInfoService.GetUserId().Returns(userId);
+            _listRepository.GetByIdAsync(command.Id).ReturnsNull();
+
+            listModel.Should().NotBeNull();
+            listModel.OwnerId.Should().Be(userId);
+            await _handler.ExecuteAsync(command);
+        }
+
+
+        [Fact]
+        public async Task handler_should_return_error_when_user_isNot_owner()
+        {
+            const long userId = 1;
+            var command = new EditListCommand
+            {
+                Id = 1,
+                Title = "new title"
+            };
+            var listModel = ListModel.Create(ListOwnerId.FromLong(100), Title.FromString("title"));
+            _userInfoService.GetUserId().Returns(1);
+            _listRepository.GetByIdAsync(command.Id).Returns(listModel);
+
+            listModel.Should().NotBeNull();
+            listModel.OwnerId.Should().NotBe(userId);
+            await _handler.ExecuteAsync(command);
         }
     }
 }
